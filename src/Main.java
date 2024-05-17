61a6e1f3fa0eb9e54eefcbda8666495be16e4313
ChesnokovEK <4echoko@gmail.com>
2024-05-17 12:20:15 +0300

    chore: изменение имени для прохождения автотестов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import Manager.Managers;
-import Manager.TaskManager;
+import manager.Managers;
+import manager.TaskManager;
 import Tasks.*;
 
 import java.util.*;
--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -1,4 +1,4 @@
-package Manager;
+package manager;
 
 import Tasks.AbstractTask;
 import Tasks.Epic;
