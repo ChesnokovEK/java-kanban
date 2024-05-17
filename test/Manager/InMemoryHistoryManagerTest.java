a5869b51d12440c1275863c91183d5aa0eb8164b
ChesnokovEK <4echoko@gmail.com>
2024-05-17 12:14:08 +0300

    fix: 1. CustomLinkedList вынесен в отдельный класс. 2. Возвращен файл тестов. 3. Добавлены недостающие тесты.

--- test/Manager/InMemoryHistoryManagerTest.java
+++ test/Manager/InMemoryHistoryManagerTest.java
@@ -73,4 +73,34 @@ class InMemoryHistoryManagerTest {
 
         assertEquals(i, manager.getHistory().size());
     }
+
+    @Test
+    public void shouldKeepOnlyLatestVersionOfTask() {
+        Task task = new Task(generateId(), "Description", "Title", State.NEW);
+        Task beforeUpdate = (Task) task.clone();
+
+        manager.add(task);
+        task.setState(State.IN_PROGRESS);
+        manager.add(task);
+        assertEquals(1, manager.getHistory().size());
+        assertEquals(task, manager.getHistory().get(0));
+        assertNotEquals(beforeUpdate, manager.getHistory().get(0));
+    }
+
+    @Test
+    public void shouldRemoveTaskFromHistory() {
+        List<Task> tasks = new ArrayList<>();
+        int TASKS_LIST_SIZE = 3;
+
+        for (int i = 0; i < TASKS_LIST_SIZE; i++) {
+            Task task = new Task(i, "Description" + i, "Title", State.NEW);
+            tasks.add(task);
+            manager.add(task);
+        }
+
+        assertEquals(TASKS_LIST_SIZE, manager.getHistory().size());
+        manager.remove(0);
+        assertNotEquals(TASKS_LIST_SIZE, manager.getHistory().size());
+        assertFalse(manager.getHistory().contains(tasks.get(0)));
+    }
 }
