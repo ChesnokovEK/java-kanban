f02c0d17f1ba5f69605be093a49d40f9699e41bb
ChesnokovEK <4echoko@gmail.com>
2024-05-17 12:13:56 +0300

    fix: 1. CustomLinkedList вынесен в отдельный класс. 2. Возвращен файл тестов. 3. Добавлены недостающие тесты.

--- src/Manager/InMemoryHistoryManager.java
+++ src/Manager/InMemoryHistoryManager.java
@@ -5,69 +5,6 @@ import Tasks.AbstractTask;
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static class CustomLinkedList {
-        private final Map<Integer, Node> tasksHistory = new HashMap<>();
-        private Node head;
-        private Node tail;
-
-        private void linkLast(AbstractTask task) {
-            Node element = new Node();
-            element.setTask(task);
-
-            if (tasksHistory.containsKey(task.getId())) {
-                removeNode(tasksHistory.get(task.getId()));
-            }
-
-            if (head == null) {
-                tail = element;
-                head = element;
-                element.setNext(null);
-                element.setPrevious(null);
-            } else {
-                element.setPrevious(tail);
-                element.setNext(null);
-                tail.setNext(element);
-                tail = element;
-            }
-
-            tasksHistory.put(task.getId(), element);
-        }
-
-        private List<AbstractTask> getTasks() {
-            List<AbstractTask> result = new ArrayList<>();
-            Node element = head;
-            while (element != null) {
-                result.add((element.getTask()));
-                element = element.getNext();
-            }
-            return result;
-        }
-
-        private void removeNode(Node node) {
-            if (node != null) {
-                tasksHistory.remove(node.getTask().getId());
-                Node previous = node.getPrevious();
-                Node next = node.getNext();
-
-                if (head == node) {
-                    head = node.getNext();
-                }
-                if (tail == node) {
-                    tail = node.getPrevious();
-                }
-                if (previous != null) {
-                    previous.setNext(next);
-                }
-                if (next != null) {
-                    next.setPrevious(previous);
-                }
-            }
-        }
-
-        private Node getNode(int id) {
-            return tasksHistory.get(id);
-        }
-    }
 
     private final CustomLinkedList list = new CustomLinkedList();
 
@@ -87,32 +24,3 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 }
 
-class Node {
-    private AbstractTask task;
-    private Node previous;
-    private Node next;
-
-    public Node getNext() {
-        return next;
-    }
-
-    public Node getPrevious() {
-        return previous;
-    }
-
-    public AbstractTask getTask() {
-        return task;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public void setPrevious(Node prev) {
-        this.previous = prev;
-    }
-
-    public void setTask(AbstractTask task) {
-        this.task = task;
-    }
-}
