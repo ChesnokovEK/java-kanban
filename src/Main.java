8de6a2a98b715483d356da47205360ac633be7e5
ChesnokovEK <4echoko@gmail.com>
2024-06-16 14:35:53 +0300

    fix: фикс для автотестов

--- src/Main.java
+++ src/Main.java
@@ -33,15 +33,15 @@ public class Main {
         manager.updateSubTask(firstSubtask);
         manager.getTaskById(firstTask.getId());
         manager.getTaskById(secondTask.getId());
-        System.out.println("Задачи");
+        System.out.println("Р—Р°РґР°С‡Рё");
         System.out.println(manager.getAllTasks());
-        System.out.println("Эпики");
+        System.out.println("Р­РїРёРєРё");
         System.out.println(manager.getAllEpics());
-        System.out.println("Подзадачи");
+        System.out.println("РџРѕРґР·Р°РґР°С‡Рё");
         System.out.println(manager.getAllSubTasks());
-        System.out.println("История");
+        System.out.println("РСЃС‚РѕСЂРёСЏ");
         System.out.println(manager.getHistory());
-        System.out.println("Список задач в зависимости от приоритета");
+        System.out.println("Р—Р°РґР°С‡Рё РІ РїРѕСЂСЏРґРєРµ РїСЂРёРѕСЂРёС‚РµС‚Р°");
         System.out.println(manager.getPrioritizedTasks());
     }
 }
