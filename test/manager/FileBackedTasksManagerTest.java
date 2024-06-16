7655861ad06f87cb15c43feb619314c57a6557c0
ChesnokovEK <4echoko@gmail.com>
2024-06-16 14:12:38 +0300

    test: 1. Добавлены тесты на новые методы с учетом граничных случаев

--- test/manager/FileBackedTasksManagerTest.java
+++ test/manager/FileBackedTasksManagerTest.java
@@ -3,13 +3,17 @@ package manager;
 import enums.State;
 import org.junit.jupiter.api.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
+import org.junit.jupiter.api.function.Executable;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
@@ -19,17 +23,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     File file;
     Path path;
+    LocalDateTime localDateTime;
 
     @BeforeEach
     public void beforeEach() {
         try {
             file = File.createTempFile("Test", ".csv");
             path = file.toPath();
-            manager = new FileBackedTasksManager(file);
+            manager = FileBackedTasksManager.loadFromFile(file);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
+
+
 
     @AfterEach
     public void afterEach() {
@@ -46,9 +53,9 @@ class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
         manager.createTask(firstTask);
         Task secondTask = new Task(generateId(), "Task Title", "Task Description", State.NEW);
         manager.createTask(secondTask);
-        Epic epic = new Epic(generateId(), "Epic Title", "Epic Description");
+        Epic epic = new Epic(generateId(), "Epic Title", "Epic Description", generateLocalDateTime(), 0);
         manager.createEpic(epic);
-        SubTask subtask = new SubTask(generateId(), "SubTask Title", "SubTask Description", epic.getId(), State.NEW);
+        SubTask subtask = new SubTask(generateId(), "SubTask Title", "SubTask Description", epic.getId(), generateLocalDateTime(), 0, State.NEW);
         manager.createSubTask(subtask);
         epic.addRelatedSubTask(subtask);
         assertEquals(List.of(firstTask, secondTask), manager.getAllTasks());
@@ -58,16 +65,20 @@ class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
     @Test
     public void shouldCorrectlyLoad() {
-        manager.createTask(new Task(generateId(), "Title", "Description", State.NEW));
-        manager.createTask(new Task(generateId(), "Title", "Description", State.NEW));
-        manager.createEpic(new Epic(generateId(), "Title", "Description"));
-        manager.createSubTask(new SubTask(generateId(), "Title", "Description", 2, State.NEW));
-        FileBackedTasksManager loadedManager = FileBackedTasksManager.loadFromFile(file);
+        Task firstTask = new Task(generateId(), "Task Title", "Task Description", State.NEW);
+        manager.createTask(firstTask);
+        Task secondTask = new Task(generateId(), "Task Title", "Task Description", State.NEW);
+        manager.createTask(secondTask);
+        Epic epic = new Epic(generateId(), "Epic Title", "Epic Description", generateLocalDateTime(), 0);
+        manager.createEpic(epic);
+        SubTask subtask = new SubTask(generateId(), "SubTask Title", "SubTask Description", epic.getId(), generateLocalDateTime(), 0, State.NEW);
+        manager.createSubTask(subtask);
+        epic.addRelatedSubTask(subtask);
 
         assertFalse(manager.getAllTasks().isEmpty());
-        assertEquals(manager.getAllTasks(), loadedManager.getAllTasks());
-        assertEquals(manager.getAllEpics(), loadedManager.getAllEpics());
-        assertEquals(manager.getAllSubTasks(), loadedManager.getAllSubTasks());
+        assertEquals(List.of(firstTask, secondTask), manager.getAllTasks());
+        assertEquals(List.of(subtask), manager.getAllSubTasks());
+        assertEquals(List.of(epic), manager.getAllEpics());
     }
 
     @Test
@@ -82,5 +93,116 @@ class FileBackedTasksManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     public void shouldSaveAndLoadEmptyHistory() {
         manager = FileBackedTasksManager.loadFromFile(file);
         assertEquals(Collections.EMPTY_LIST, manager.getHistory());
+    }
+
+    @Test
+    public void shouldCorrectlyCreateTaskFromString() {
+        Task task = new Task(
+                0,
+                "Task-1",
+                "Task-1 Description",
+                State.NEW,
+                LocalDateTime.parse("2023-01-01T00:00"),
+                40
+        );
+        Epic epic = new Epic(
+                1,
+                "Epic-1",
+                "Epic-1 Description",
+                LocalDateTime.parse("2022-01-01T00:00"),
+                40
+        );
+        SubTask subTask = new SubTask(
+                2,
+                "SubTask-1",
+                "SubTask-1 Description",
+                1,
+                LocalDateTime.parse("2021-01-01T00:00"),
+                40,
+                State.NEW
+        );
+        epic.addRelatedSubTask(subTask);
+
+        final String taskString = "0,TASK,Task-1,NEW,Task-1 Description,2023-01-01T00:00,40,";
+        final String SubTaskString = "2,SUBTASK,SubTask-1,NEW,SubTask-1 Description,2021-01-01T00:00,40,1";
+        final String epicString = "1,EPIC,Epic-1,NEW,Epic-1 Description,2022-01-01T00:00,40,";
+
+        Path tempPath = Path.of("tempData.csv");
+        File tempFile = new File(String.valueOf(tempPath));
+
+        try (FileWriter writer = new FileWriter(String.valueOf(tempFile))) {
+            writer.write(System.lineSeparator());
+            writer.write(taskString + System.lineSeparator());
+            writer.write(epicString + System.lineSeparator());
+            writer.write(SubTaskString + System.lineSeparator());
+            writer.write(System.lineSeparator());
+        } catch (IOException e) {
+            throw new ManagerSaveException("Не удалось сохранить в файл", e);
+        }
+
+        manager = FileBackedTasksManager.loadFromFile(tempFile);
+
+        assertEquals(List.of(task), manager.getAllTasks());
+        assertEquals(List.of(subTask), manager.getAllSubTasks());
+        assertEquals(List.of(epic), manager.getAllEpics());
+    }
+
+    @Test
+    public void shouldCorrectlyFindIntersections(){
+        Task task = new Task(
+                0,
+                "Task-1",
+                "Task-1 Description",
+                State.NEW,
+                LocalDateTime.parse("2020-01-01T00:00"),
+                40
+        );
+        Task overlapTask = new Task(
+                3,
+                "Task-1",
+                "Task-1 Description",
+                State.NEW,
+                LocalDateTime.parse("2019-12-31T00:00"),
+                60L*24+1
+        );
+        Epic epic = new Epic(
+                1,
+                "Epic-1",
+                "Epic-1 Description",
+                LocalDateTime.parse("2019-01-01T00:00"),
+                40
+        );
+        SubTask subTask = new SubTask(
+                2,
+                "SubTask-1",
+                "SubTask-1 Description",
+                1,
+                LocalDateTime.parse("2018-01-01T00:00"),
+                40,
+                State.NEW
+        );
+        epic.addRelatedSubTask(subTask);
+        manager.createTask(task);
+        manager.createTask(overlapTask);
+        manager.createEpic(epic);
+        manager.createSubTask(subTask);
+
+        assertTrue(manager.getPrioritizedTasks().contains(task));
+        assertTrue(manager.getPrioritizedTasks().contains(subTask));
+        assertTrue(manager.getPrioritizedTasks().contains(epic));
+        assertFalse(manager.getPrioritizedTasks().contains(overlapTask));
+    }
+
+    @Test
+    public void shouldThrowSaveException() {
+        Path tempPath = Path.of("wrongFile.csv");
+        File tempFile = new File(String.valueOf(tempPath));
+
+        try {
+            Files.deleteIfExists(tempPath);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        assertThrows(ManagerSaveException.class, () -> FileBackedTasksManager.loadFromFile(tempFile));
     }
 }
--- test/manager/TaskManagerTest.java
+++ test/manager/TaskManagerTest.java
@@ -7,6 +7,8 @@ import tasks.Task;
 import enums.*;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,16 +21,20 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         return id++;
     }
 
+    public LocalDateTime generateLocalDateTime() {
+        return LocalDateTime.now().plusMinutes(new Random().nextLong(10000L));
+    }
+
     public Task createTask() {
         return new Task(generateId(), "Title", "Description", State.NEW);
     }
 
     public Epic createEpic() {
-        return new Epic(generateId(), "Title", "Description");
+        return new Epic(generateId(), "Title", "Description", generateLocalDateTime(), 0);
     }
 
     public SubTask createSubTask(Epic epic) {
-        SubTask subTask = new SubTask(generateId(), "Title", "Description", epic.getId(), State.NEW);
+        SubTask subTask = new SubTask(generateId(), "Title", "Description", epic.getId(), generateLocalDateTime(), 0, State.NEW);
         epic.addRelatedSubTask(subTask);
         return subTask;
     }
@@ -42,13 +48,14 @@ public abstract class TaskManagerTest<T extends TaskManager> {
 
     @Test
     public void shouldCreateTask() {
-
+        Task taskwithOnlyTitleAndDescription = new Task ("Title of task", "Description of task");
         Task task = createTask();
         manager.createTask(task);
+        manager.createTask(taskwithOnlyTitleAndDescription);
         Collection<Task> tasks = manager.getAllTasks();
         assertNotNull(tasks);
         assertEquals(State.NEW, task.getState());
-        assertEquals(task, manager.getTaskById(task.getId()));
+        assertEquals(List.of(task, taskwithOnlyTitleAndDescription), manager.getAllTasks());
     }
 
     @Test
@@ -60,6 +67,15 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         assertNotNull(epic.getState());
         assertEquals(State.NEW, epic.getState());
         assertEquals(epic, manager.getEpicById(epic.getId()));
+    }
+
+    @Test
+    public void shouldAssignNewIdIfSameIdExists() {
+        Epic firstEpic = new Epic(0, "Title-1", "Description", generateLocalDateTime(), 0);
+        Epic secondEpic = new Epic(0, "Title-2", "Description", generateLocalDateTime(), 0);
+        manager.createEpic(firstEpic);
+        manager.createEpic(secondEpic);
+        assertNotEquals(manager.getAllEpics().get(0).getId(), manager.getAllEpics().get(1).getId());
     }
 
     @Test
@@ -93,15 +109,28 @@ public abstract class TaskManagerTest<T extends TaskManager> {
     public void shouldUpdateEpicStateFromSubTaskState() {
         Epic epic = new Epic("Epic-Title", "Description");
         manager.createEpic(epic);
-        SubTask subTask = new SubTask(generateId(), "SubTask-Title", "Description", epic.getId(), State.NEW);
-        manager.createSubTask(subTask);
+        // a. Все подзадачи со статусом NEW.
+        SubTask firstSubTask = new SubTask(generateId(), "SubTask-Title", "Description", epic.getId(), generateLocalDateTime(), 0,  State.NEW);
+        SubTask secondSubTask = new SubTask(generateId(), "SubTask-Title", "Description", epic.getId(), generateLocalDateTime(), 0,  State.NEW);
+        manager.createSubTask(firstSubTask);
+        manager.createSubTask(secondSubTask);
         assertEquals(State.NEW, manager.getEpicById(epic.getId()).getState());
-        subTask.setState(State.IN_PROGRESS);
-        manager.updateSubTask(subTask);
-        assertEquals(manager.getSubTaskById(subTask.getId()).getState(), manager.getEpicById(epic.getId()).getState());
-        subTask.setState(State.DONE);
-        manager.updateSubTask(subTask);
+        // b. Все подзадачи со статусом DONE.
+        firstSubTask.setState(State.DONE);
+        secondSubTask.setState(State.DONE);
+        manager.updateSubTask(firstSubTask);
+        manager.updateSubTask(secondSubTask);
         assertEquals(State.DONE, manager.getEpicById(epic.getId()).getState());
+        // c. Подзадачи со статусами NEW и DONE.
+        firstSubTask.setState(State.NEW);
+        manager.updateSubTask(firstSubTask);
+        assertEquals(State.IN_PROGRESS, manager.getEpicById(epic.getId()).getState());
+        // d. Подзадачи со статусом IN_PROGRESS.
+        firstSubTask.setState(State.IN_PROGRESS);
+        secondSubTask.setState(State.IN_PROGRESS);
+        manager.updateSubTask(firstSubTask);
+        manager.updateSubTask(secondSubTask);
+        assertEquals(State.IN_PROGRESS, manager.getEpicById(epic.getId()).getState());
     }
 
     @Test
@@ -321,7 +350,7 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         Epic epic = createEpic();
         manager.createEpic(epic);
         SubTask subtask1 = createSubTask(epic);
-        SubTask subtask2 = new SubTask(1, "Title", "Description", 1, State.NEW);
+        SubTask subtask2 = new SubTask(1, "Title", "Description", 1, generateLocalDateTime(), 0, State.NEW);
         manager.createSubTask(subtask1);
         manager.createSubTask(subtask2);
         assertEquals(1, manager.getAllSubTasks().size());
@@ -338,5 +367,110 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         manager.updateTask(task1);
         manager.getTaskById(task1.getId());
         assertNotEquals(List.of(task2, task1), manager.getHistory());
+    }
+
+    @Test
+    public void shouldCorrectlyRemoveEpicRelatedSubtasks() {
+        Epic epic = new Epic(
+                999,
+                "Epic-1 Title",
+                "Description",
+                generateLocalDateTime(),
+                1
+        );
+        SubTask firstSubTask = new SubTask(
+                1000,
+                "Subtask-1 Title",
+                "Description",
+                999,
+                LocalDateTime.now().plusMinutes(20),
+                1,
+                State.NEW
+        );
+        SubTask secondSubTask = new SubTask(
+                1001,
+                "Subtask-1 Title",
+                "Description",
+                999,
+                LocalDateTime.now().plusMinutes(10),
+                1,
+                State.NEW
+        );
+        manager.createEpic(epic);
+        manager.createSubTask(firstSubTask);
+        manager.createSubTask(secondSubTask);
+        assertTrue(manager.getAllSubTasks().size() > 0);
+        manager.removeEpic(999);
+        assertEquals(0, manager.getAllSubTasks().size());
+        manager.createEpic(epic);
+        manager.createSubTask(firstSubTask);
+        manager.createSubTask(secondSubTask);
+        assertTrue(manager.getAllSubTasks().size() > 0);
+        manager.removeAllEpics();
+        assertEquals(0, manager.getAllSubTasks().size());
+    }
+
+    @Test
+    public void shouldCorrectlyPrintTasksInTerminal() {
+        Task task = createTask();
+        Epic epic = createEpic();
+        SubTask subTask = createSubTask(epic);
+        epic.addRelatedSubTask(subTask);
+
+        List<Integer> subTasksId = new ArrayList<>();
+
+        epic.getAllRelatedSubTasks().stream().forEach(subtask -> subTasksId.add(subtask.getId()));
+
+        assertEquals(System.lineSeparator() + "Task {" + System.lineSeparator()
+                + "\tid='" + task.getId() + "'"
+                + System.lineSeparator() + "\ttitle='" + task.getTitle() + "'"
+                + ", " + System.lineSeparator() + "\tdescription='" + task.getDescription() + "'"
+                + ", " + System.lineSeparator() + "\tstate='" + task.getState() + "'"
+                + ", " + System.lineSeparator() + "\tstartTime='" + task.getStartTime() + "'"
+                + ", " + System.lineSeparator() + "\tendTime='" + task.getEndTime() + "'"
+                + ", " + System.lineSeparator() + "\tduration='" + task.getDuration().toMinutes() + "'"
+                + System.lineSeparator() + "}", task.toString());
+
+        assertEquals(System.lineSeparator() + "SubTask {" + System.lineSeparator()
+                + "\tid='" + subTask.getId() + "'"
+                + System.lineSeparator() + "\ttitle='" + subTask.getTitle() + "'"
+                + ", " + System.lineSeparator() + "\tdescription='" + subTask.getDescription() + "'"
+                + ", " + System.lineSeparator() + "\tstate='" + subTask.getState() + "'"
+                + ", " + System.lineSeparator() + "\trelatedEpicId='" + subTask.getRelatedEpicId() + "'"
+                + ", " + System.lineSeparator() + "\tstartTime='" + subTask.getStartTime() + "'"
+                + ", " + System.lineSeparator() + "\tendTime='" + subTask.getEndTime() + "'"
+                + ", " + System.lineSeparator() + "\tduration='" + subTask.getDuration().toMinutes() + "'"
+                + System.lineSeparator() + "}", subTask.toString());
+
+        assertEquals(System.lineSeparator() + "Epic {" + System.lineSeparator()
+                + "\tid='" + epic.getId() + "'"
+                + System.lineSeparator()+"\ttitle='" + epic.getTitle() + "'"
+                + ", " + System.lineSeparator() + "\tdescription='" + epic.getDescription() + "'"
+                + ", " + System.lineSeparator() + "\tstate='" + epic.getState() + "'"
+                + ", " + System.lineSeparator() + "\tstartTime='" + epic.getStartTime() + "'"
+                + ", " + System.lineSeparator() + "\tendTime='" + epic.getEndTime() + "'"
+                + ", " + System.lineSeparator() + "\tduration='" + epic.getDuration().toMinutes() + "'"
+                + ", " + System.lineSeparator() + "\trelatedSubTasksId=" + subTasksId + System.lineSeparator() + "}", epic.toString());
+    }
+
+    @Test
+    public void shouldCorrectlyCalculateHash() {
+        Task firstTask = new Task("Title", "Description");
+        Task secondTask = new Task("Title", "Description");
+        Epic firstEpic = new Epic("Title", "Description");
+        Epic secondEpic = new Epic("Title", "Description");
+        SubTask firstSubTask = new SubTask("Title", "Description", firstEpic.getId());
+        SubTask secondSubTask = new SubTask("Title", "Description", firstEpic.getId());
+
+        assertEquals(firstTask.hashCode(), secondTask.hashCode());
+        assertEquals(firstEpic.hashCode(), secondEpic.hashCode());
+        assertEquals(firstSubTask.hashCode(), secondSubTask.hashCode());
+        firstEpic.addRelatedSubTask(firstSubTask);
+        firstEpic.addRelatedSubTask(secondSubTask);
+        secondTask.setStartTime(LocalDateTime.now().plusMinutes( 40L));
+        secondSubTask.setDuration(Duration.ZERO.plusMinutes( 40L));
+        assertNotEquals(firstTask.hashCode(), secondTask.hashCode());
+        assertNotEquals(firstSubTask.hashCode(), secondSubTask.hashCode());
+        assertNotEquals(firstEpic.hashCode(), secondEpic.hashCode());
     }
 }
